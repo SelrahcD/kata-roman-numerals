7ed3deb849f7d0b15993b442d31f88eb3431c596
SelrahcD <c.desneuf@gmail.com>
2022-07-11 19:41:42 +0200

    Install dependencies

--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -22,7 +22,7 @@ final class RomanNumeralConverter
 
         foreach (self::ARABIC_TO_ROMAN AS $arabic => $numeral) {
 
-            if($arabicNumber % $arabic == $arabicNumber - $arabic) {
+            if($arabicNumber !== $arabic && $arabicNumber % $arabic == $arabicNumber - $arabic) {
                 echo $arabicNumber . PHP_EOL;
             }
             if($arabicNumber >= 4 && $arabicNumber < 5) { // 5 1 -> 5 % 4 => 1 && 5 - 4 = 1
