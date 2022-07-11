e6f91a28ca963a19cceaf2674f492aeb61e5b585
SelrahcD <c.desneuf@gmail.com>
2022-07-11 18:04:24 +0200

    Install dependencies

--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -36,7 +36,7 @@ final class RomanNumeralConverter
             return $result;
         }
 
-        if($arabicNumber >= 5) {
+        if($arabicNumber >= 5 && $arabicNumber < 10) {
             $result .= 'V';
             $arabicNumber = $arabicNumber - 5;
         }
