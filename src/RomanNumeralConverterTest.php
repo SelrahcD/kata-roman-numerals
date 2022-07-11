5b17811c8ab5fab362dea03feef7a9b1a18542ef
SelrahcD <c.desneuf@gmail.com>
2022-07-11 18:07:41 +0200

    Install dependencies

--- src/RomanNumeralConverterTest.php
+++ src/RomanNumeralConverterTest.php
@@ -68,6 +68,13 @@ final class RomanNumeralConverterTest extends TestCase
     /**
      * @test
      */
+    public function convert_12_to_XII(): void {
+        $this->assertEquals('XII', (new RomanNumeralConverter)->toRomanNumerals(12));
+    }
+
+    /**
+     * @test
+     */
     public function convert_50_to_L(): void {
         $this->assertEquals('L', (new RomanNumeralConverter)->toRomanNumerals(50));
     }
