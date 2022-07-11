4891ca0c557600a466f51c0eea388d2f380cff84
SelrahcD <c.desneuf@gmail.com>
2022-07-11 18:38:04 +0200

    Install dependencies

--- src/RomanNumeralConverterTest.php
+++ src/RomanNumeralConverterTest.php
@@ -9,156 +9,178 @@ use PHPUnit\Framework\TestCase;
 final class RomanNumeralConverterTest extends TestCase
 {
     /**
-    * @test
-    */
-    public function convert_1_to_I(): void {
+     * @test
+     */
+    public function convert_1_to_I(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('I', 1);
     }
 
     /**
      * @test
      */
-    public function convert_2_to_II(): void {
+    public function convert_2_to_II(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('II', 2);
     }
 
     /**
      * @test
      */
-    public function convert_3_to_III(): void {
+    public function convert_3_to_III(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('III', 3);
     }
 
     /**
      * @test
      */
-    public function convert_5_to_V(): void {
+    public function convert_5_to_V(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('V', 5);
     }
 
     /**
      * @test
      */
-    public function convert_6_to_VI(): void {
+    public function convert_6_to_VI(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('VI', 6);
     }
 
     /**
      * @test
      */
-    public function convert_7_to_VII(): void {
+    public function convert_7_to_VII(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('VII', 7);
     }
 
     /**
      * @test
      */
-    public function convert_10_to_X(): void {
+    public function convert_10_to_X(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('X', 10);
     }
 
     /**
      * @test
      */
-    public function convert_11_to_XI(): void {
+    public function convert_11_to_XI(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XI', 11);
     }
 
     /**
      * @test
      */
-    public function convert_20_to_XX(): void {
+    public function convert_20_to_XX(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XX', 20);
     }
 
     /**
      * @test
      */
-    public function convert_21_to_XXI(): void {
+    public function convert_21_to_XXI(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XXI', 21);
     }
 
     /**
      * @test
      */
-    public function convert_26_to_XXVI(): void {
+    public function convert_26_to_XXVI(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XXVI', 26);
     }
 
     /**
      * @test
      */
-    public function convert_12_to_XII(): void {
+    public function convert_12_to_XII(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XII', 12);
     }
 
     /**
      * @test
      */
-    public function convert_15_to_XV(): void {
+    public function convert_15_to_XV(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XV', 15);
     }
 
     /**
      * @test
      */
-    public function convert_16_to_XVI(): void {
+    public function convert_16_to_XVI(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('XVI', 16);
     }
 
     /**
      * @test
      */
-    public function convert_50_to_L(): void {
+    public function convert_50_to_L(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('L', 50);
     }
 
     /**
      * @test
      */
-    public function convert_51_to_LI(): void {
+    public function convert_51_to_LI(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('LI', 51);
     }
 
     /**
      * @test
      */
-    public function convert_60_to_LX(): void {
+    public function convert_60_to_LX(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('LX', 60);
     }
 
     /**
      * @test
      */
-    public function convert_100_to_C(): void {
+    public function convert_100_to_C(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('C', 100);
     }
 
     /**
      * @test
      */
-    public function convert_115_to_C(): void {
+    public function convert_115_to_C(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('CXV', 115);
     }
 
     /**
      * @test
      */
-    public function convert_500_to_D(): void {
+    public function convert_500_to_D(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('D', 500);
     }
 
     /**
      * @test
      */
-    public function convert_600_to_D(): void {
+    public function convert_600_to_D(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('DC', 600);
     }
 
     /**
      * @test
      */
-    public function convert_1000_to_M(): void {
+    public function convert_1000_to_M(): void
+    {
         $this->converts_from_arabic_numbers_to_roman_numeral('M', 1000);
     }
 
@@ -176,6 +198,29 @@ final class RomanNumeralConverterTest extends TestCase
 
     public function numbers()
     {
-        return;
+        return [
+            ['I', 1],
+            ['II', 2],
+            ['III', 3],
+            ['V', 5],
+            ['VI', 6],
+            ['VII', 7],
+            ['X', 10],
+            ['XI', 11],
+            ['XX', 20],
+            ['XXI', 21],
+            ['XXVI', 26],
+            ['XII', 12],
+            ['XV', 15],
+            ['XVI', 16],
+            ['L', 50],
+            ['LI', 51],
+            ['LX', 60],
+            ['C', 100],
+            ['CXV', 115],
+            ['D', 500],
+            ['DC', 600],
+            ['M', 1000],
+        ];
     }
 }
