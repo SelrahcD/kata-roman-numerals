5f74af91aa74d8da6509021aab77cae9ddcdcec5
SelrahcD <c.desneuf@gmail.com>
2022-07-12 19:07:22 +0200

    Install dependencies

--- src/RomanNumeralConverterTest.php
+++ src/RomanNumeralConverterTest.php
@@ -10,60 +10,95 @@ final class RomanNumeralConverterTest extends TestCase
 {
 
     /**
-    * @test
-    */
-    public function convert_1_to_I(): void {
-        $this->assertEquals('I', (new RomanNumeralConverter)->toRomanNumerals(1));
+     * @test
+     */
+    public function convert_1_to_I(): void
+    {
+        $this->assertConversion('I', 1);
     }
 
     /**
      * @test
      */
-    public function convert_2_to_II(): void {
-        $this->assertEquals('II', (new RomanNumeralConverter)->toRomanNumerals(2));
+    public function convert_2_to_II(): void
+    {
+        $this->assertConversion('II', 2);
     }
 
     /**
      * @test
      */
-    public function convert_3_to_III(): void {
-        $this->assertEquals('III', (new RomanNumeralConverter)->toRomanNumerals(3));
+    public function convert_3_to_III(): void
+    {
+        $this->assertConversion('III', 3);
     }
 
     /**
      * @test
      */
-    public function convert_4_to_IV(): void {
-        $this->assertEquals('IV', (new RomanNumeralConverter)->toRomanNumerals(4));
+    public function convert_4_to_IV(): void
+    {
+        $this->assertConversion('IV', 4);
     }
 
     /**
      * @test
      */
-    public function convert_5_to_V(): void {
-        $this->assertEquals('V', (new RomanNumeralConverter)->toRomanNumerals(5));
+    public function convert_5_to_V(): void
+    {
+        $this->assertConversion('V', 5);
     }
 
     /**
      * @test
      */
-    public function convert_6_to_VI(): void {
-        $this->assertEquals('VI', (new RomanNumeralConverter)->toRomanNumerals(6));
+    public function convert_6_to_VI(): void
+    {
+        $this->assertConversion('VI', 6);
     }
 
 
     /**
      * @test
      */
-    public function convert_8_to_VIII(): void {
-        $this->assertEquals('VIII', (new RomanNumeralConverter)->toRomanNumerals(8));
+    public function convert_8_to_VIII(): void
+    {
+        $this->assertConversion('VIII', 8);
     }
 
     /**
      * @test
      */
-    public function convert_9_to_IX(): void {
-        $this->assertEquals('IX', (new RomanNumeralConverter)->toRomanNumerals(9));
+    public function convert_9_to_IX(): void
+    {
+        $this->assertConversion('IX', 9);
+    }
+
+    /**
+     * @param string $romanNumeral
+     * @param int $arabicNumber
+     * @return void
+     */
+    private function assertConversion(string $romanNumeral, int $arabicNumber): void
+    {
+        $this->assertEquals($romanNumeral, (new RomanNumeralConverter)->toRomanNumerals($arabicNumber));
+    }
+
+    public function numbers(): array
+    {
+        $data = [
+            ['I', 1],
+            ['II', 2],
+            ['III', 3],
+            ['IV', 4],
+            ['V', 5],
+            ['VI', 6],
+            ['VIII', 8],
+            ['IX', 9],
+        ];
+
+        return array_reduce($data, function (array $result, $datum) {
+     });
     }
 
 
