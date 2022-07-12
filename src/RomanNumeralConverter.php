2e4b2a9e09d3dd8496233d0cd24af470ed587232
SelrahcD <c.desneuf@gmail.com>
2022-07-12 18:59:43 +0200

    Install dependencies

--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -7,7 +7,8 @@ namespace RomanNumerals;
 final class RomanNumeralConverter
 {
     private const MAPPING = [
-        4 => 'IV'
+        4 => 'IV',
+        5 => 'V'
     ];
 
     public function toRomanNumerals(int $arabicNumber): string
@@ -16,7 +17,7 @@ final class RomanNumeralConverter
         if($arabicNumber === 5) {
             return 'V';
         }
-        
+
         $allAs1 = str_repeat('I', $arabicNumber);
 
         return str_replace(str_repeat('I', 4), 'IV', $allAs1);
