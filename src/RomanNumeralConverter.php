ca981546058769e5355cfe6273cd845f398280b3
SelrahcD <c.desneuf@gmail.com>
2022-07-12 08:57:04 +0200

    Roman numerals

--- src/RomanNumeralConverter.php
+++ src/RomanNumeralConverter.php
@@ -6,5 +6,32 @@ namespace RomanNumerals;
 
 final class RomanNumeralConverter
 {
+    const ARABIC_TO_ROMAN = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',
+    ];
 
+    public function toRomanNumerals(int $arabicNumber)
+    {
+        $result = '';
+
+        foreach (self::ARABIC_TO_ROMAN AS $arabic => $numeral) {
+            $repetition = intdiv($arabicNumber, $arabic);
+            $result .= str_repeat($numeral, $repetition);
+            $arabicNumber = $arabicNumber - $arabic * $repetition;
+        }
+        
+        return $result;
+    }
 }
--- src/RomanNumeralConverterTest.php
+++ src/RomanNumeralConverterTest.php
@@ -8,11 +8,52 @@ use PHPUnit\Framework\TestCase;
 
 final class RomanNumeralConverterTest extends TestCase
 {
-
     /**
-    * @test
-    */
-    public function convert_1_to_I(): void {
-        $this->assertEquals('I', (new RomanNumeralConverter)->toRomanNumerals(1));
+     * @test
+     * @dataProvider numbers
+     */
+    public function converts_from_arabic_numbers_to_roman_numeral(string $roman, int $arabicNumber): void
+    {
+        $this->assertEquals($roman, (new RomanNumeralConverter)->toRomanNumerals($arabicNumber));
+    }
+
+    public function numbers()
+    {
+        return [
+            "I <=> 1" => ['I', 1],
+            "II <=> 2" => ['II', 2],
+            "III <=> 3" => ['III', 3],
+            "IV <=> 4" => ['IV', 4],
+            "V <=> 5" => ['V', 5],
+            "VI <=> 6" => ['VI', 6],
+            "VII <=> 7" => ['VII', 7],
+            "IX <=> 9" => ['IX', 9],
+            "X <=> 10" => ['X', 10],
+            "XI <=> 11" => ['XI', 11],
+            "XX <=> 20" => ['XX', 20],
+            "XXI <=> 21" => ['XXI', 21],
+            "XXVI <=> 26" => ['XXVI', 26],
+            "XII <=> 12" => ['XII', 12],
+            "XV <=> 15" => ['XV', 15],
+            "XVI <=> 16" => ['XVI', 16],
+            "XL <=> 40" => ['XL', 40],
+            "XLIII <=> 43" => ['XLIII', 43],
+            "L <=> 50" => ['L', 50],
+            "LI <=> 51" => ['LI', 51],
+            "LX <=> 60" => ['LX', 60],
+            "LXX <=> 70" => ['LXX', 70],
+            "XC <=> 90" => ['XC', 90],
+            "C <=> 100" => ['C', 100],
+            "CXV <=> 115" => ['CXV', 115],
+            "CC <=> 200" => ['CC', 200],
+            "CD <=> 400" => ['CD', 400],
+            "CDXVII <=> 417" => ['CDXVII', 417],
+            "D <=> 500" => ['D', 500],
+            "DC <=> 600" => ['DC', 600],
+            "CM <=> 900" => ['CM', 900],
+            "M <=> 1000" => ['M', 1000],
+            "MCMIII <=> 1903" => ['MCMIII', 1903],
+            "MMM <=> 3000" => ['MMM', 3000],
+        ];
     }
 }
